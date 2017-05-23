dce1c8a8758a6952f50ec2841d81ce42a549ed71
Cristian Jara Figueroa <crisjf@mit.edu>
2017-05-23 14:20:54 -0400

    finished songs_metadata

--- songs_metadata.py
+++ songs_metadata.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
 import json,requests,pandas as pd
 song_ids = set(pd.read_csv('songs.tsv',delimiter='\t',header=None)[2])
-song_ids = list(song_ids)[:10]
 
 fields = ['tracking_data','release_date_components','release_date','stats','annotation_count','comment_count','title',
           'spotify_uuid','url','recording_location','primary_artist','featured_artists','producer_artists',
@@ -14,13 +13,6 @@ for song_id in song_ids:
 		r = requests.get(url)
 		data = defaultdict(lambda:'NULL',r.json()['response']['song'])
 		data_out = {key:data[key] for key in fields}
-		#data_out['tracking_data'] = {val['key']:val['value'] for val in data_out['tracking_data']}
-		#data_out['primary_artist'] = data_out['primary_artist']['api_path']
-		#data_out['producer_artists'] = [a['api_path'] for a in data_out['producer_artists']]
-		#data_out['writer_artists'] = [a['api_path'] for a in data_out['writer_artists']]
-		#data_out['album'] = { key:data_out['album'][key] for key in ['release_date','api_path','url','name','artist']}
-		#data_out['album']['artist'] = data_out['album']['artist']['api_path']
-		#data_out['description'] = data_out['description']['dom']['children']
 		outfile.write( (json.dumps(data_out)+'\n').encode('utf-8'))
 	except:
 		g.write(song_id+'\n')
