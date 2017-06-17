148d5bb7bc215a180f788e3e8dfca6562e49646f
Cristian Jara Figueroa <crisjf@mit.edu>
2017-06-17 18:53:25 -0400

    Updated readme

--- annotations_content.py
+++ annotations_content.py
@@ -1,1 +1,6 @@
 #Place holder
+#This function should:
+#Retrieves the content for all the annotations in songs_annotations.tsv and
+#Save it as independent .tsv files (one for each song, with the song_id as the file name) in annotations/.
+#Each file has the format:
+#annotation_id	annotation_content
