43f67860c0b4d916095a5635292101813760d378
Cristian Jara Figueroa <crisjf@mit.edu>
2017-08-15 21:46:11 -0400

    added lyrics.py

---
+++ lyrics.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from functions import *
+data = pd.read_csv('processed_data/data_song_annotation_merged_20170815_cleaned.csv',encoding='utf-8')
+for i in data['Song ID'].values:
+    f = open('lyrics/'+str(int(i))+'.txt',mode='w')
+    song_id = '/songs/'+str(int(i))
+    song_url = song_id2url(song_id)
+    f.write(song_lyrics(song_url).encode('utf-8'))
+    f.close()
