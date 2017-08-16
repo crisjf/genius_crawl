283b6722e457c4e966ea7b8e78422dce93adf4a1
Cristian Jara Figueroa <crisjf@mit.edu>
2017-08-15 21:38:54 -0400

    languages fixed

--- languages.py
+++ languages.py
@@ -14,13 +14,12 @@ def to_list(s):
     else:
         return sp
 
-data = pd.read_csv('processed_data/data_song_annotation_merged_20170815.csv',encoding='utf-8').sample(1000)
+data = pd.read_csv('processed_data/data_song_annotation_merged_20170815.csv',encoding='utf-8')
 print len(data)
 
 data['lang'] = 'NULL'
 data['is_null'] = True
 data['len_annot'] = 0
-print len(data[data['lang']=='NULL'])
 for index,row in data.iterrows():
     s = to_list(row['annotations'])
     data.loc[index,'len_annot'] = len(s)
@@ -30,6 +29,5 @@ for index,row in data.iterrows():
             data.loc[index,'is_null'] = False
         except:
             pass
-
 print len(data[data['lang']=='NULL'])
 data.to_csv('processed_data/data_song_annotation_merged_20170815_withlangs.csv',index=False,encoding='utf-8')
