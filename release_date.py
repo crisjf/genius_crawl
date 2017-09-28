112550271c671ad835eaa0ecf58b026aa5ad5e64
Cristian Jara Figueroa <crisjf@mit.edu>
2017-09-27 21:13:41 -0400

    added exception to release_date

--- release_date.py
+++ release_date.py
@@ -12,14 +12,17 @@ f.close()
 
 for line in lines:
 	if line.strip() != '':
-		song = json.loads(line)
-		song_id = None
-		release = song['release_date']
-		for i in song['tracking_data']:
-			if i['key'] == 'Song ID':
-				song_id = i['value']
-				break
-		if (song_id is not None)&(release is not None):
-			out.append((song_id,release))
+		try:
+			song = json.loads(line)
+			song_id = None
+			release = song['release_date']
+			for i in song['tracking_data']:
+				if i['key'] == 'Song ID':
+					song_id = i['value']
+					break
+			if (song_id is not None)&(release is not None):
+				out.append((song_id,release))
+		except:
+			pass
 out = pd.DataFrame(out,columns=['Song ID','release_date'])
 out.to_csv('processed_data/release_date.csv',index=False,encoding='utf-8')
