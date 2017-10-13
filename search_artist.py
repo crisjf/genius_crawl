49de8c07f283db70cabd54961798fb73403510f9
Cristian Jara Figueroa <crisjf@mit.edu>
2017-10-13 11:30:33 -0400

    added instance set to search_artist

--- search_artist.py
+++ search_artist.py
@@ -3,6 +3,8 @@ import johnny5 as j5,pandas as pd
 data = pd.read_csv('processed_data/data_song_annotation_merged_20170815_cleaned_classified.csv',encoding='utf-8')
 data = data[data['prob']>0.5]
 
+instances = set(['Human','Band (rock and pop)','Musical ensemble','Girl group','Boy band','Side project','Double act','Musical collective','Trio (music)','Pop duo','Cast recording','Quartet','Dance troupe','Collective'])
+
 f = open('processed_data/artist2wiki.tsv',mode='w')
 f.write('Primary Artist\tPrimary Artist ID\tWiki Title\twdid\tL\tinstance_of\n')
 for artist,artist_id in data[['Primary Artist','Primary Artist ID']].drop_duplicates().values:
@@ -12,9 +14,12 @@ for artist,artist_id in data[['Primary Artist','Primary Artist ID']].drop_duplic
         a.find_article()
         if a.title() is None:
             a = j5.search(artist)
-            out += [a.title(),a.wdid(),str(a.L()),j5.article(a.wd_prop('P31')[0]['id']).title()]
-        else:
-            out += [a.title(),a.wdid(),str(a.L()),j5.article(a.wd_prop('P31')[0]['id']).title()]
+        for i in a.wd_prop('P31'):
+            if j5.article(i['id']).title() in instances:
+                out += [a.title(),a.wdid(),str(a.L()),j5.article(i['id']).title()]
+                break
+        if len(out) == 2:
+            out += ['NULL','NULL','NULL','NULL']
     except:
         out += ['NULL','NULL','NULL','NULL']
     out = [(val if val is not None else 'NULL') for val in out ]
